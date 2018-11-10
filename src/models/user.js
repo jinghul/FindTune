74407a27341195fa218012a1260826f750a622dd
Jinghu Lei <jinghul@bu.edu>
2018-11-10 13:56:44 -0500

    finish mongodb tests

--- src/models/user.js
+++ src/models/user.js
@@ -2,19 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Create schema and model
-
-const UserSchema = new Schema({
-    name: String, // user name, required
-    profile : String, // user id from Spotify, required
-    // preferences : [PreferenceSchema] // preference array based on category
-});
-
 const PreferenceSchema = new Schema({
     name : String, // name of the category
     weight : Number, // weight of the genre for the user, required: -1 <= x <= 1, -1 bad, 1 good
     categoryid : String // categoryID, required – maps to the genre cache
 });
 
+const UserSchema = new Schema({
+    name: String, // user name, required
+    profile : String, // user id from Spotify, required
+    preferences : [PreferenceSchema] // preference array based on category
+});
 
 const User = mongoose.model('user', UserSchema);
 
--- test/db_test.js
+++ test/db_test.js
@@ -25,10 +25,10 @@ after(function() {
 });
 
  // Tests	
-describe("Database tests...", function() {	
+describe("Database tests...", async function() {	
     
     // Create tests	
-    it("Adding a record...", function(done) {	
+    it("Adding a basic record", function(done) {	
         var user = new User({
             name : "Test User",
             profile : "Test profile"
@@ -37,6 +37,115 @@ describe("Database tests...", function() {
         user.save().then(function() {
             assert(user.isNew === false);
             done();
+        }).catch(function() {
+            mongoose.connection.close();
         });
     });	
+
+    it("Adding a record with nested preferences", function(done) {
+        var user = new User({
+            name : "Patrick",
+            preferences: [{name:"pop", weight:0.78, categoryid:"abcdefg"}]
+        });
+
+        user.save().then(function() {
+            User.findOne({name : "Patrick"}).then(function(result) {
+                assert(result.preferences[0].name === "pop")
+                done();
+            }).catch(done);
+        });
+    });
+
+    it("Adding preference to an user", function(done) {
+        var user = new User({
+            name : "Patrick2",
+            preferences: [{name:"pop", weight:0.78, categoryid:"abcdefg"}]
+        });
+
+        user.save().then(function() {
+            User.findOne({name:"Patrick2"}).then(function(result) {
+                result.preferences.push({name : "tophits", weight : 0.22});
+                result.save().then(function() {
+                    assert(result.preferences.length === 2)
+                    done();
+                });
+            }).catch(done);
+        });
+    });
+});
+
+describe('Finding records...', function() {
+    
+    var user;
+
+    this.beforeEach(function(done) {
+        user = new User({
+            name : "FindUser",
+            profile : "findusrprof"
+        });
+
+         user.save().then(function() {
+            done();
+         });
+    });
+
+    it('Find one record by name', function(done) {
+        User.findOne({name:'FindUser'}).then(function(result) {
+            assert(result.name === 'FindUser');
+            done();
+        });
+    });
+
+    it('Find one record by id', function(done) {
+        User.findOne({_id : user._id}).then(function(result) {
+            assert(result._id.toString() === user._id.toString());
+            done();
+        });
+    });
+});
+
+describe("Deleting records...", function() {
+    var user;
+
+    this.beforeEach(function(done) {
+        user = new User({
+            name : "DeleteUser",
+        });
+
+         user.save().then(function() {
+            done();
+         });
+    });
+
+    it('Deletes one record from database', function(done) {
+        User.findOneAndDelete({name:'DeleteUser'}).then(function() {
+            User.findOne({name:"DeleteUser"}).then(function(result) {
+                assert(result === null);
+                done();
+            });
+        });
+    });
+});
+
+describe("Updating records...", function() {
+    var user;
+
+    this.beforeEach(function(done) {
+        user = new User({
+            name : "UpdateUser",
+        });
+
+         user.save().then(function() {
+            done();
+         });
+    });
+
+    it('Updates one record from database', function(done) {
+        User.findOneAndUpdate({name:'UpdateUser'}, {name:'Updated'}).then(function() {
+            User.findOne({_id:user.id}).then(function(result) {
+                assert(result.name === "Updated");
+                done();
+            });
+        });
+    });
 });
