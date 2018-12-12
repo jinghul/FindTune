37a28cf18615687b39b8dc406fe6a4784998e1d8
Jinghu Lei <jinghul@bu.edu>
2018-12-11 22:03:03 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
 /* Import express modules and routes */
-<<<<<<< HEAD
-const express = require("express");
-const auth = require("./routes/auth");
-const token = require("./routes/token");
-const play = require("./routes/play");
-
-/* Utility Modules*/
-const path = require("path");
-const utils = require("./utils");
-const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
-const secret_key = require("../keys.json").session_secret;
-=======
 const express = require('express');
 const auth = require('./routes/auth');
 const play = require('./routes/play');
@@ -26,7 +13,6 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const config = require('./config');
 const secret_key = config.keys.session_secret;
->>>>>>> frontend
 
 /* Database Initialization */
 const mongoose = require("mongoose");
@@ -34,22 +20,6 @@ require("./database"); // connect to database
 
 /* Node Server and Routes Initialization */
 var app = express();
-<<<<<<< HEAD
-app.use(express.static(path.join(__dirname, "/public")));
-
-// Sessions
-app.use(
-  session({
-    genid: req => {
-      // Generates new sessionID if it is not matched.
-      return utils.generateRandomString(16);
-    },
-    store: new MongoStore({ mongooseConnection: mongoose.connection }), // or use a new connection
-    secret: secret_key,
-    resave: false,
-    saveUninitialized: true
-  })
-=======
 app.use(helmet());
 app.use(express.static(path.join(__dirname, '../dist/')));
 
@@ -65,24 +35,12 @@ app.use(
         resave: false,
         saveUninitialized: true,
     })
->>>>>>> frontend
 );
 
 // Index, loads user cookie info ex. name, when visiting play/profile refresh token,
 // if error, invoke login again, and save redirect url to take back to it.
 
 // Routes
-<<<<<<< HEAD
-app
-  .use("/login", auth)
-  .use("/token", token)
-  .use("/play", play);
-//.use(face)
-
-const port = process.env.PORT || 8888;
-app.listen(port, () => {
-  console.log("Listening on port " + port + "...");
-=======
 app.use('/login', auth).use('/play', play).use('/face', face).use('/profile', profile);
 
 // Status 404 (Error) middleware
@@ -93,5 +51,4 @@ app.use('*', function(req,res){
 
 app.listen(config.app.port, () => {
     console.log('Listening on port ' + config.app.port + '...');
->>>>>>> frontend
 });
